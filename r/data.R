824d0cbdb2c3e3daac451e6b20007e4bc49a7703
nlhopkins <natashahopkins1@hotmail.co.uk>
2023-03-16 21:12:37 +0000

    update

--- r/data.R
+++ r/data.R
@@ -2,16 +2,17 @@ library(tidyverse)
 library(hrbrthemes)
 library(eulerr)
 
+
 raw <- read.delim("data/raw/hg19.txt") %>%
-    janitor::clean_names() %>%
-    mutate(across(contains("ed"), ~ .x / ed_input)) %>%
-    mutate(across(contains("dox"), ~ .x / dox_input)) %>%
-    mutate(fox_fold = (dox_foxa1_high - ed_foxa1) / ed_foxa1) %>%
-    mutate(h3_fold = (dox_h3k27ac - ed_h3k27ac) / ed_h3k27ac)
+    janitor::clean_names()
 
 threshold <- median(raw$ed_h3k27ac)
 
 raw <- raw %>%
+    mutate(across(contains("ed"), ~ .x / ed_input)) %>%
+    mutate(across(contains("dox"), ~ .x / dox_input)) %>%
+    mutate(fox_fold = (dox_foxa1_high - ed_foxa1) / ed_foxa1) %>%
+    mutate(h3_fold = (dox_h3k27ac - ed_h3k27ac) / ed_h3k27ac) %>%
     mutate(fox_status = if_else(dox_foxa1_high > ed_foxa1, "gain", "loss")) %>%
     mutate(h3_status = ifelse(
         ed_h3k27ac < threshold & dox_h3k27ac > threshold,
@@ -20,9 +21,32 @@ raw <- raw %>%
                    dox_h3k27ac < threshold, 'loss', 'shared')
     ))
 
-library(eulerr)
-fit <- euler(c(A = 450, B = 1800, "A&B" = 230))
-plot(fit)
+
+upstream <- read.delim("data/raw/upstream.txt") %>%
+    janitor::clean_names() %>%
+    mutate(position = "upstream") %>%
+    mutate(across(contains("ed"), ~ .x / ed_input)) %>%
+    mutate(across(contains("dox"), ~ .x / dox_input)) %>%
+    mutate(fox_fold = (dox_foxa1_high - ed_foxa1) / ed_foxa1) %>%
+    mutate(h3_fold = (dox_h3k27ac - ed_h3k27ac) / ed_h3k27ac) %>%
+    mutate(fox_status = if_else(dox_foxa1_high > ed_foxa1, "gain", "loss")) %>%
+    mutate(h3_status = ifelse(
+        ed_h3k27ac < threshold & dox_h3k27ac > threshold,
+        'gain',
+        ifelse(ed_h3k27ac > threshold &
+                   dox_h3k27ac < threshold, 'loss', 'shared')
+    ))
+
+downstream <- read.delim("data/raw/downstream.txt") %>%
+    janitor::clean_names() %>%
+    mutate(position = "downstream")
+
+
+total <- full_join(upstream,
+                   downstream,
+                   by = "name",
+                   suffix = c(".up", ".dn"))
+
 
 ggplot(raw, aes(x = log10(fox_fold), y = log10(h3_fold))) +
     geom_point(color = "black") +
@@ -65,3 +89,34 @@ euler(c(A = fox_gain, B = h3_gain, "A&B" = gain_gain)) %>% plot
 
 
 euler(c(A = fox_loss, B = h3_loss, "A&B" = loss_loss)) %>% plot
+
+
+
+
+
+# foxa1
+raw %>%
+    pivot_longer(cols = contains(c("_foxa1", "_h3k27ac")),
+                 names_to = "treatment") %>%
+    filter(!grepl("h3k27ac|dox_foxa1_low", treatment)) %>%
+    ggplot(aes(x = value, fill = treatment)) +
+    geom_histogram(color = "#e9ecef",
+                   alpha = 0.6,
+                   position = 'identity') +
+    scale_fill_manual(values = c("#69b3a2", "#404080")) +
+    theme_ipsum() +
+    labs(fill = "")
+
+
+# h3k27ac
+raw %>%
+    pivot_longer(cols = contains(c("_foxa1", "_h3k27ac")),
+                 names_to = "treatment") %>%
+    filter(!grepl("foxa1", treatment)) %>%
+    ggplot(aes(x = value, fill = treatment)) +
+    geom_histogram(color = "#e9ecef",
+                   alpha = 0.6,
+                   position = 'identity') +
+    scale_fill_manual(values = c("#69b3a2", "#404080")) +
+    theme_ipsum() +
+    labs(fill = "")

3ba56e9fe4f8645d772d05fe4f222d16153db451
nlhopkins <natashahopkins1@hotmail.co.uk>
2023-03-30 01:10:34 +0100

    Update

--- r/barchart.R
+++ r/barchart.R
@@ -1,21 +1,48 @@
 load('environments/data.RData')
 
-total %>%
-    mutate(activity = if_else(value > total_threshold, "active", "inactive")) %>%
+x <- data %>%
+    filter(grepl("ed_h3k27ac|dox_h3k27ac", treatment)) %>%
+    mutate(h3_status = case_when(value > threshold ~ "active", TRUE ~ "inactive")) %>%
+    group_by(treatment, h3_status) %>%
+    mutate(active_count = n() / 2) %>%
+    ungroup() %>%
+    group_by(treatment) %>%
+    mutate(gene_count = n() / 2) %>%
+    ungroup()
+
+
+
+x %>% ggplot(aes(
+    fill = h3_status,
+    x = treatment,
+    y = active_count / gene_count
+)) +
+    geom_bar(position = "dodge", stat = "identity") +
+    xlab("") +
+    scale_fill_discrete(name = "") +
+    theme_ipsum() +
+    scale_y_continuous(limits = c(0, 0.6),
+                       labels = scales::percent)
+
+
+
+## IGNORE !!! ACTIVITY APPLIED TO FOX FIX
+##
+## total %>%
+mutate(activity = if_else(value > total_threshold, "active", "inactive")) %>%
     filter(!grepl("dox_foxa1_low", treatment)) %>%
     filter(!grepl("inactive", activity)) %>%
-    mutate(target = if_else(grepl("foxa1", treatment), "fox", "h3k27ac")) %>%
     mutate(treatment = if_else(grepl("ed", treatment), "ed", "dox")) %>%
     ggplot(aes(
         fill = factor(position, level = c("upstream", "downstream")),
         x = factor(treatment, level = c("ed", "dox")),
-        y = after_stat(count / sum(count / 2))
+        y = after_stat(count / sum(count / 4))
     )) +
     geom_bar(position = "dodge") +
     xlab("") +
     scale_fill_discrete(name = "") +
     theme_ipsum() +
-    facet_grid( ~ treatment) +
-    scale_y_continuous(labels = percent)
+    facet_grid(~ target) +
+    scale_y_continuous(labels = scales::percent)
 
 save.image(file = 'environments/barchart.RData')
--- r/data.R
+++ r/data.R
@@ -8,17 +8,17 @@ library(purrr)
 library(ggpmisc)
 library(ggstatsplot)
 
+options(scipen = 999)
+
 #### Tidy ####
 raw <- read.delim("data/raw/hg19.txt") %>%
     janitor::clean_names()
-
-threshold <- median(raw$ed_h3k27ac, na.rm = TRUE)
 
 data <- raw %>%
     mutate(across(contains("ed"), ~ .x / ed_input)) %>%
     mutate(across(contains("dox"), ~ .x / dox_input)) %>%
     mutate(fox_fold = (dox_foxa1_high - ed_foxa1) / ed_foxa1) %>%
-    mutate(h3_fold = (dox_h3k27ac - ed_h3k27ac) / ed_h3k27ac) %>%
+    mutate(h3_fold = (dox_h3k27ac - ed_h3k27ac) / ed_h3k27ac)  %>%
     mutate(activity = ifelse(
         ed_h3k27ac < threshold & dox_h3k27ac > threshold,
         'gain',
@@ -28,12 +28,24 @@ data <- raw %>%
     mutate(fox_abs = dox_foxa1_high - ed_foxa1) %>%
     mutate(h3_abs = dox_h3k27ac - ed_h3k27ac) %>%
     pivot_longer(cols = contains(c("_foxa1", "_h3k27ac")),
-                 names_to = "treatment")
+                 names_to = "treatment") %>%
+    mutate(condition = str_extract(treatment, "[^_]+"))
+
+threshold <- data %>%
+    group_by(treatment) %>%
+    summarise_at("value", median) %>%
+    filter(treatment == "ed_h3k27ac") %>%
+    pull(2)
+
+
+
 
 
 #### fox volcano ####
 volcano_fox <- data %>%
-    pivot_wider(names_from = "treatment", values_from = "value") %>%
+    pivot_wider(names_from = "treatment",
+                values_from = "value",
+                id_cols = "name") %>%
     select(c(name, ed_foxa1, dox_foxa1_high)) %>%
     mutate(p_value = -log10(pmap_dbl(select(., -1),  ~ chisq.test(c(
         ...
@@ -56,7 +68,9 @@ volcano_fox$diffexpressed[volcano_fox$fox_fold < -0.5 &
 
 #### h3 volcano ####
 volcano_h3 <- data %>%
-    pivot_wider(names_from = "treatment", values_from = "value") %>%
+    pivot_wider(names_from = "treatment",
+                values_from = "value",
+                id_cols = "name") %>%
     select(c(name, ed_h3k27ac, dox_h3k27ac)) %>%
     mutate(p_value = -log10(pmap_dbl(select(., -1),  ~ chisq.test(c(
         ...
@@ -79,7 +93,8 @@ volcano_h3$diffexpressed[volcano_h3$h3_fold < -0.5 &
     "DOWN"
 
 
-diffexpressed <- merge(volcano_fox, volcano_h3, by = "name") %>%
+diffexpressed <-
+    merge(volcano_fox, volcano_h3, by = "name") %>%
     pivot_longer(cols = contains(c("diffexpressed.x", "diffexpressed.y")),
                  names_to = "comparison",
                  values_to = "diffexpressed") %>%
@@ -119,14 +134,14 @@ loss_gain <-
 
 
 up <- merge(volcano_fox, volcano_h3, by = "name") %>%
-    filter(diffexpressed.x == "UP" & diffexpressed.y == "UP")
+    filter(diffexpressed.x == "UP" &
+               diffexpressed.y == "UP")
 
 dn <- merge(volcano_fox, volcano_h3, by = "name") %>%
-    filter(diffexpressed.x == "DOWN" & diffexpressed.y == "DOWN")
+    filter(diffexpressed.x == "DOWN" &
+               diffexpressed.y == "DOWN")
 
 
-
-save.image(file = 'environments/data.RData')
 #### relative position ####
 upstream <- read.delim("data/raw/upstream.txt") %>%
     janitor::clean_names() %>%
@@ -140,12 +155,17 @@ downstream <- read.delim("data/raw/downstream.txt") %>%
 total <- full_join(upstream,
                    downstream) %>%
     mutate(across(contains("ed"), ~ .x / ed_input)) %>%
-    mutate(across(contains("dox"), ~ .x / dox_input))
-
-total_threshold <- median(total$ed_h3k27ac, na.rm = TRUE)
-
-total <- total %>%
+    mutate(across(contains("dox"), ~ .x / dox_input)) %>%
     pivot_longer(cols = contains(c("_foxa1", "_h3k27ac")),
                  names_to = "treatment")
 
 total <- merge(diffexpressed, total, by = "name")
+
+total_threshold <- data %>%
+    group_by(treatment) %>%
+    summarise_at(vars(value), list(name = median)) %>%
+    filter(treatment == "ed_h3k27ac") %>%
+    pull(2)
+
+save.image(file = 'environments/data.RData')
+
